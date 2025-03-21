181ce2b5ed2c3e8e4245380b0f53481c23202ae1
Dieter <contactdieter@gmail.com>
2025-03-21 10:02:49 +0100

    upload af flere filer som f eks classdiagram

---
+++ Project Genspil/Game.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Genspil
+{
+    class Game
+    {
+
+        // Field
+        // private string name;
+        // Property
+        public string Name { get; set; }
+        public string Genre { get; private set; }
+        public int Players { get; private set; }
+        public int Age { get; private set; }
+        public double Price { get; private set; }
+        public char Condition { get; private set; }
+        public bool Requested { get; private set; } //efterspørgsel
+        public int Stockstatus { get; private set; }
+
+        
+        public Game(string name, string genre, int players, int age, double price, char condition, bool requested, int stockstatus)
+        {
+            Name = name;
+            Genre = genre;
+            Players = players;
+            Age = age;
+            Price = price;
+            Condition = condition;
+            Requested = requested;
+            Stockstatus = stockstatus + 1;
+            //year = _year;
+ 
+        }
+
+
+        public void getData()
+        {
+            Console.WriteLine($"the game is " + Name);
+        }
+
+        public void setName(string name)
+        {
+            Name = name;
+        }
+
+    }
+}
--- Project Genspil/Program.cs
+++ Project Genspil/Program.cs
@@ -1,10 +1,60 @@
-﻿namespace Project_Genspil
+﻿using System.Net.Http.Headers;
+
+namespace Project_Genspil
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            /*string retry = "No";
+            Console.WriteLine("Choose an option:");
+            Console.WriteLine("1. Print list");
+            Console.WriteLine("2. Search Games");
+            Console.WriteLine("3. Choose whatever\n");*/
+            //Game Matador = new Game("Matador", "Eventyr", 4, 8, 49.99, 'B', true, 2);
+
+            List<Game> games = new List<Game>(); // == var game = new List<Game>();
+            games.Add(new Game("Matador", "Eventyr", 4, 8, 49.99, 'B', true, 2));
+            games.Add(new Game("Matador", "Eventyr", 4, 8, 49.99, 'B', true, 2));
+            for (int i = 0; i < )
+            {
+
+            }
+
+            /*Console.WriteLine(Matador.Name);
+            Matador.getData();
+            Matador.setName("monopoli");
+            Matador.getData();*/
+
+
+                /*
+                //do
+                while (retry != "No") ;
+                {
+                    Console.Write("Your number: ");
+                    int menu = int.Parse(Console.ReadLine());
+
+                    switch (menu)
+                    {
+                        case 1:
+                            Console.WriteLine("You chose: Print List");
+                            break;
+                        case 2:
+                            Console.WriteLine("You chose: Search Games");
+                            break;
+                        case 3:
+                            Console.WriteLine("You chose: Choose whatever");
+                            break;
+                        default:
+                            Console.WriteLine("Du har tastet forkert.");
+                            Console.WriteLine("Vil du prøve igen?");
+                            retry = Console.ReadLine();
+                            break;
+                    }
+                }
+                //while (retry != "No");
+                */
+            Console.ReadKey();
         }
     }
 }
